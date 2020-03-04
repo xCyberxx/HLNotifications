be9ed59909040d88dd2205de17c6faaeaf029e72
SS <Prettycooldude@gmail.com>
2020-03-04 19:23:33 +0100

    Initial Releas

---
+++ HLNotification/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace HLNotification
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkTmr = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Notifications: OFF";
+            // 
+            // checkTmr
+            // 
+            this.checkTmr.Tick += new System.EventHandler(this.checkTmr_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(120, 79);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Highlight Notification";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer checkTmr;
+    }
+}
+
---
+++ HLNotification/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace HLNotification
+{
+    public partial class Form1 : Form
+    {
+
+        bool isRunning = false;
+        string hightlightPath = "Temp\\Highlights\\Escape From Tarkov";
+
+        int count = 0;
+        bool initCount = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            isRunning = !isRunning;
+            if(isRunning)
+            {
+                label1.Text = "Notifications: ON";
+                button1.Text = "Stop";
+                checkTmr.Start();
+            }
+            else
+            {
+                label1.Text = "Notifications: OFF";
+                button1.Text = "Start";
+                checkTmr.Stop();
+                initCount = false;
+            }
+
+        }
+
+        private void checkTmr_Tick(object sender, EventArgs e)
+        {
+            if(!initCount)
+            {
+                //set count to amount of files
+                count = Directory.GetFiles(hightlightPath, "*").Length;
+                initCount = true;
+            }
+
+            int nCount = Directory.GetFiles(hightlightPath, "*").Length;
+
+            if(nCount > count)
+            {
+                Console.Beep();
+            }
+
+            count = nCount;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            hightlightPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), hightlightPath);
+        }
+    }
+}
